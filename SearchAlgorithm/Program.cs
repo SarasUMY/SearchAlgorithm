bbde563a448d262a314d65760b12ea48f8d1e962
TA1092 <bagas.saras.ft21@mail.umy.ac.id>
2022-10-26 16:07:21 +0700

    Membuat variabel int i dan int n

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+
+        public void input()
+        {
+
+        }
+    }
+}
